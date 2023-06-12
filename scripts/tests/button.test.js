1f9dc3288cbec39b4df4707bae2512717273823a
Hanmb17 <>
2023-06-12 18:29:14 +0000

    add more DOM testing

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -4,8 +4,12 @@
 
 const buttonClick = require("../button");
 
-beforeEach ( () => {
-    document.body.innerHTML = "<p id='par'></p>";
+beforeAll(() => {
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
 });
 
 describe("DOM tests", () => {
@@ -13,5 +17,8 @@ describe("DOM tests", () => {
         buttonClick();
         expect(document.getElementById("par")
             .innerHTML).toEqual("You Clicked");
+    });
+    test("h1 should exist", () => {
+        expect(document.getElementsByTagName("h1").length).toBe(1);
     });
 });
