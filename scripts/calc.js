b6b965da8d3df3422d9abad9ffa4308b7dadad06
Hanmb17 <>
2023-06-12 18:04:21 +0000

    second jest test

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,5 +1,11 @@
-function addition() {
-    return 42;
+// first pass test but fails on second test
+//function addition() {
+  //   return 42;
+//}
+
+
+function addition(a, b) {
+  return a + b;
 }
 
 module.exports = addition;
--- scripts/tests/calc.test.js
+++ scripts/tests/calc.test.js
@@ -1,9 +1,14 @@
 const addition = require("../calc");
-
+ // first test 
 describe("Calculator", () => {
     describe("Addition function", () => {
+        // first test 
         test("should return 42 for 20 + 22", () => {
             expect(addition(20, 22)).toBe(42);
+        });
+        // second test 
+        test("should return 73 for 42 +31", () => {
+            expect(addition(42, 31)).toBe(73)
         });
     });
     describe("Subtract function", () => {
